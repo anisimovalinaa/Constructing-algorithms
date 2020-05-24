017e7e120634969967370db8f47e5eee77b56573
anisimovalinaa <anisimova.linaa@yandex.ru>
2020-05-24 20:42:51 +0300

    Update

--- Индивидуальное задание 2. База данных/Индивидуальное задание 2. База данных/Program.cs
+++ Индивидуальное задание 2. База данных/Индивидуальное задание 2. База данных/Program.cs
@@ -19,5 +19,16 @@ namespace Индивидуальное_задание_2.База_данных
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());          
         }
+
+        public static MySqlConnection Conn ()
+        {
+            string host = "127.0.0.1"; // Имя локального компьютера
+            string database = "insurance company"; // Имя базы данных
+            string user = "root"; // Имя пользователя
+
+            string Connect = "Database=" + database + ";Datasource=" + host + ";User=" + user;
+            MySqlConnection mysql_connection = new MySqlConnection(Connect);
+            return mysql_connection;
+        }
     }
 }
--- Индивидуальное задание 2. База данных/Индивидуальное задание 2. База данных/Tab1.Designer.cs
+++ Индивидуальное задание 2. База данных/Индивидуальное задание 2. База данных/Tab1.Designer.cs
@@ -31,11 +31,13 @@
             this.table = new System.Windows.Forms.DataGridView();
             this.showClients = new System.Windows.Forms.Button();
             this.addClient = new System.Windows.Forms.Button();
-            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.adress = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sex = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dateOfBirth = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pasport = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.jobPlace = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.address = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.phoneNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.inn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.table)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,20 +46,22 @@
             this.table.AllowUserToAddRows = false;
             this.table.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.table.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.id,
             this.name,
-            this.adress,
-            this.phoneNumber,
-            this.inn});
+            this.sex,
+            this.dateOfBirth,
+            this.pasport,
+            this.jobPlace,
+            this.address,
+            this.phoneNumber});
             this.table.Location = new System.Drawing.Point(12, 12);
             this.table.Name = "table";
-            this.table.Size = new System.Drawing.Size(456, 283);
+            this.table.Size = new System.Drawing.Size(793, 283);
             this.table.TabIndex = 0;
             this.table.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // showClients
             // 
-            this.showClients.Location = new System.Drawing.Point(591, 38);
+            this.showClients.Location = new System.Drawing.Point(724, 55);
             this.showClients.Name = "showClients";
             this.showClients.Size = new System.Drawing.Size(162, 23);
             this.showClients.TabIndex = 1;
@@ -67,7 +71,7 @@
             // 
             // addClient
             // 
-            this.addClient.Location = new System.Drawing.Point(591, 85);
+            this.addClient.Location = new System.Drawing.Point(724, 84);
             this.addClient.Name = "addClient";
             this.addClient.Size = new System.Drawing.Size(162, 23);
             this.addClient.TabIndex = 2;
@@ -75,38 +79,47 @@
             this.addClient.UseVisualStyleBackColor = true;
             this.addClient.Click += new System.EventHandler(this.addClient_Click);
             // 
-            // id
-            // 
-            this.id.HeaderText = "id";
-            this.id.Name = "id";
-            this.id.ReadOnly = true;
-            this.id.Width = 20;
-            // 
             // name
             // 
             this.name.HeaderText = "ФИО";
             this.name.Name = "name";
+            this.name.Width = 150;
             // 
-            // adress
+            // sex
             // 
-            this.adress.HeaderText = "Адрес";
-            this.adress.Name = "adress";
+            this.sex.HeaderText = "Пол";
+            this.sex.Name = "sex";
+            // 
+            // dateOfBirth
+            // 
+            this.dateOfBirth.HeaderText = "Дата рождения";
+            this.dateOfBirth.Name = "dateOfBirth";
+            // 
+            // pasport
+            // 
+            this.pasport.HeaderText = "Паспорт";
+            this.pasport.Name = "pasport";
+            // 
+            // jobPlace
+            // 
+            this.jobPlace.HeaderText = "Место работы";
+            this.jobPlace.Name = "jobPlace";
+            // 
+            // address
+            // 
+            this.address.HeaderText = "Адрес";
+            this.address.Name = "address";
             // 
             // phoneNumber
             // 
             this.phoneNumber.HeaderText = "Номер телефона";
             this.phoneNumber.Name = "phoneNumber";
             // 
-            // inn
-            // 
-            this.inn.HeaderText = "ИНН";
-            this.inn.Name = "inn";
-            // 
             // Tab1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(924, 450);
             this.Controls.Add(this.addClient);
             this.Controls.Add(this.showClients);
             this.Controls.Add(this.table);
@@ -122,10 +135,12 @@
         private System.Windows.Forms.DataGridView table;
         private System.Windows.Forms.Button showClients;
         private System.Windows.Forms.Button addClient;
-        private System.Windows.Forms.DataGridViewTextBoxColumn id;
         private System.Windows.Forms.DataGridViewTextBoxColumn name;
-        private System.Windows.Forms.DataGridViewTextBoxColumn adress;
+        private System.Windows.Forms.DataGridViewTextBoxColumn sex;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dateOfBirth;
+        private System.Windows.Forms.DataGridViewTextBoxColumn pasport;
+        private System.Windows.Forms.DataGridViewTextBoxColumn jobPlace;
+        private System.Windows.Forms.DataGridViewTextBoxColumn address;
         private System.Windows.Forms.DataGridViewTextBoxColumn phoneNumber;
-        private System.Windows.Forms.DataGridViewTextBoxColumn inn;
     }
 }
